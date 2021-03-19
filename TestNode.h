e97377089443db644dabc6e97d6d57d7dd147eb0
milutinB <milutin.brankovic3@gmail.com>
2021-03-19 11:58:19 +1100

    first commit

---
+++ TestNode.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <algorithm>
+#include "AbstractNode.h"
+
+namespace AVLTest {
+    class TestNode: public AbstractNode<int> {
+
+        private:
+
+        public:
+            TestNode() {
+                AbstractNode<int>::left = nullptr;
+                AbstractNode<int>::right = nullptr;
+                AbstractNode<int>::parent = nullptr;
+                AbstractNode<int>::key = 0;
+                AbstractNode<int>::height = 1;
+            };
+
+            TestNode(int _key) {
+                AbstractNode<int>::left = nullptr;
+                AbstractNode<int>::right = nullptr;
+                AbstractNode<int>::parent = nullptr;
+                AbstractNode<int>::key = _key;
+                AbstractNode<int>::height = 1;
+            }
+    };
+
+}
---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include "FirstLayerNode.h"
+#include "TestNode.h"
+#include <vector>
+#include "SegmentDS.h"
+#include <stdlib.h> 
+#include <unordered_set>
+#include <math.h>
+
+#define PI 3.141592653589793238462643383279502884197169
+
+int main() {
+    // auto avl_tree = new LeafDataAVL<AbstractNode<int>>();
+
+    // std::vector<int> integers{436, 1234523, 252345,32545,2345235,23452,23452,4324,23234,12341,65745,3452,67856,432534,756,2432354,413,78567,25234,8756,43523,8678,
+    // 436, 1234523, 252345,32545,2345235,23452,23452,4324,23234,12341,65745,3452,67856,432534,756,2432354,413,78567,25234,8756,43523,8678};
+
+    // std::vector<AVLTest::TestNode*> nodes;
+
+    // for (auto n: integers)
+    //     nodes.push_back(new AVLTest::TestNode(n));
+
+    // for (auto node: nodes) {
+    //     avl_tree->insert(node);
+    //     avl_tree->post_order(avl_tree->get_root());
+    //     std::cout << "end traversal\n";
+    // }
+
+    // avl_tree->post_order(avl_tree->get_root());
+    srand (12345);
+
+    auto segs = std::vector<SegmentDS::Segment*>();
+
+    auto x_coords = std::unordered_set<double>();
+    auto y_coords = std::unordered_set<double>();
+
+    // for (int i = 0; i < 10000; ++i) {
+    //     auto x1 = rand() % 1000000;
+        
+    //     while (x_coords.find(x1) != x_coords.end())
+    //         x1 = rand() % 1000000;
+        
+    //     x_coords.emplace(x1);
+
+    //     auto x2 = rand() % 1000000;
+    //     while (x_coords.find(x2) != x_coords.end())
+    //         x2 = rand() % 1000000;
+
+    //     x_coords.emplace(x2);
+
+    //     auto y = rand() % 1000000;
+    //     while (y_coords.find(y) != y_coords.end())
+    //         y = rand() % 1000000;
+
+    //     y_coords.emplace(y);
+
+    //     double theta = PI / 4;
+
+    //     segs.push_back(new Segment(x1*cos(theta) - y*sin(theta), x1*sin(theta) + y* cos(theta), 
+    //     x2*cos(theta) - (y+0.0001)*sin(theta), x2*sin(theta) + (y+0.0001)* cos(theta)));
+    // }
+
+    for (int i = 0; i < 1000000; ++i) {
+        segs.push_back(new SegmentDS::Segment((i+1)*(0-(1/(i+1))), (i+1)*(0 + i), (i+1)*(1-1/(i+1)), (i+1)*(1 + i)));
+    }
+
+    std::cout << "start\n";
+    auto segDS = SegmentDS::SegmentDS(segs);
+    std::cout << "done\n";
+    segDS.count_segs();
+    std::cout << "segs in DS: " << SegmentDS::seg_count << std::endl;
+}
